432dcb59d280c66bccb913655f926d5a351a2e2a
Javier Nieto <javiermnieto89@gmail.com>
2023-09-09 00:08:26 -0500

    init fft lagrange coefficients

--- src/utils.go
+++ src/utils.go
@@ -4,13 +4,18 @@ import (
 	"math/big"
 
 	"github.com/consensys/gnark-crypto/ecc/bls12-381/fr"
+	"github.com/consensys/gnark-crypto/ecc/bls12-381/fr/fft"
+)
+
+var (
+	Zero = fr.NewElement(0)
 )
 
 type Message []byte
 
 // This function returns the lagrange coefficients for a given set of indices when evaluated at a specific point
 // TODO: Have to optimize this
-func GetLagAt(at fr.Element, indices []fr.Element) []fr.Element {
+func GetLagAtSlow(at fr.Element, indices []fr.Element) []fr.Element {
 	n := len(indices)
 	results := make([]fr.Element, n)
 
@@ -35,6 +40,48 @@ func GetLagAt(at fr.Element, indices []fr.Element) []fr.Element {
 		results[i].Div(&nume, &deno)
 	}
 	return results
+}
+
+// This function returns the lagrange coefficients for a given set of indices when evaluated at a specific point
+func GetLagAt(at fr.Element, T []fr.Element) []fr.Element {
+	n := len(T)
+
+	// Z(at) = \prod_{i in T} (at - \omega^i)
+	Zat := fr.One()
+	div := make([]fr.Element, n)
+	for i := 0; i < n; i++ {
+		div[i].Sub(&at, &T[i])
+		Zat.Mul(&Zat, &div[i])
+	}
+
+	// Z(X) = \prod_{i in T} (X - \omega^i)
+	Z := GetCoefficientsFromRoots(T)
+
+	// Z'(X) = \sum_{i in T} \prod_{j in T, j != i} (X - \omega^j)
+	Differentiate(&Z)
+
+	dom := fft.NewDomain(uint64(n))
+	ZPrime := make([]fr.Element, dom.Cardinality)
+	for i := uint64(copy(ZPrime, Z)); i < dom.Cardinality; i++ {
+		ZPrime[i] = fr.NewElement(0)
+	}
+	fft.BitReverse(ZPrime)
+	dom.FFT(ZPrime, fft.DIT)
+
+	denominators := make([]fr.Element, n)
+	for i := 0; i < n; i++ {
+		denominators[i] = ZPrime[i]
+	}
+	denominators = fr.BatchInvert(denominators)
+	div = fr.BatchInvert(div)
+
+	var nume fr.Element
+	for i := 0; i < n; i++ {
+		nume.Mul(&Zat, &div[i])
+		ZPrime[i].Mul(&nume, &denominators[i])
+	}
+
+	return ZPrime[:n]
 }
 
 // This function is not generic. I will use the Prod(x-hi)=x^n-1
@@ -95,4 +142,66 @@ func GetOmega(n, seed int) fr.Element {
 		return GetOmega(n, seed+1)
 	}
 	return y
+}
+
+// TODO: Optimize further
+func GetCoefficientsFromRoots(roots []fr.Element) []fr.Element {
+	if len(roots) == 0 {
+		return []fr.Element{}
+	}
+	if len(roots) == 1 {
+		// (X - roots[0])
+		c := new(fr.Element).Neg(&roots[0])
+		return []fr.Element{*c, fr.One()}
+	}
+
+	m := len(roots) / 2
+	left := GetCoefficientsFromRoots(roots[:m])
+	right := GetCoefficientsFromRoots(roots[m:])
+	if len(left) == 0 {
+		return right
+	}
+	if len(right) == 0 {
+		return left
+	}
+	return MulPolynomials(left, right)
+}
+
+// Differentiate gets the derivative of the polynomial p inplace
+func Differentiate(p *[]fr.Element) {
+	ps := *p
+	n := len(ps) - 1
+	for i := 0; i < n; i++ {
+		ps[i].Mul(&ps[i+1], new(fr.Element).SetUint64(uint64(i+1)))
+	}
+	*p = ps[:n]
+}
+
+func MulPolynomials(left, right []fr.Element) []fr.Element {
+	dom := fft.NewDomain(uint64(len(left) + len(right) - 1))
+	n := int(dom.Cardinality)
+	lNew := make([]fr.Element, n)
+	for i := copy(lNew, left); i < n; i++ {
+		lNew[i] = fr.NewElement(0)
+	}
+	rNew := make([]fr.Element, n)
+	for i := copy(rNew, right); i < n; i++ {
+		rNew[i] = fr.NewElement(0)
+	}
+
+	dom.FFT(lNew, fft.DIF)
+	dom.FFT(rNew, fft.DIF)
+	for i := 0; i < n; i++ {
+		lNew[i].Mul(&lNew[i], &rNew[i])
+	}
+	dom.FFTInverse(lNew, fft.DIT)
+	//? truncate zeros
+	// for i := n - 1; i >= 0; i-- {
+	// 	if lNew[i].IsZero() {
+	// 		lNew = lNew[:i]
+	// 	} else {
+	// 		break
+	// 	}
+	// }
+	return lNew
 }
--- src/utils_test.go
+++ src/utils_test.go
@@ -1,1 +1,86 @@
 package wts
+
+import (
+	"testing"
+
+	"github.com/consensys/gnark-crypto/ecc/bls12-381/fr"
+	"github.com/consensys/gnark-crypto/ecc/bls12-381/fr/fft"
+)
+
+func TestGetLagAt(t *testing.T) {
+	n := 12
+
+	dom := fft.NewDomain(uint64(n))
+	omega := dom.Generator
+	omegas := make([]fr.Element, n)
+	omegas[0] = fr.One()
+	for i := 1; i < n; i++ {
+		omegas[i].Mul(&omega, &omegas[i-1])
+	}
+
+	var tau fr.Element
+	tau.SetRandom()
+
+	expected := GetLagAtSlow(tau, omegas)
+	actual := GetLagAt(tau, omegas)
+
+	for i := 0; i < n; i++ {
+		if !actual[i].Equal(&expected[i]) {
+			t.Errorf("%d: Expected %s, got %s", i, expected[i].String(), actual[i].String())
+		}
+	}
+}
+
+func TestGetCoefficientsFromRoots(t *testing.T) {
+	// (X-1)(X-2)(X-3)(X-4)(X-5)
+	roots := []fr.Element{newElem(1), newElem(2), newElem(3), newElem(4), newElem(5)}
+	// X^5 - 15X^4 + 85X^3 - 225X^2 + 274X - 120
+	expected := []fr.Element{newElem(-120), newElem(274), newElem(-225), newElem(85), newElem(-15), newElem(1)}
+
+	actual := GetCoefficientsFromRoots(roots)
+	for i := 0; i < len(expected); i++ {
+		if !actual[i].Equal(&expected[i]) {
+			t.Errorf("%d: Expected %s, got %s", i, expected[i].String(), actual[i].String())
+		}
+	}
+}
+
+func TestMulPolynomials(t *testing.T) {
+	// f(x) = 1 + 2x + 3x^2 + 4x^3
+	f := []fr.Element{newElem(1), newElem(2), newElem(3), newElem(4)}
+	// g(x) = 5 + 6x + 7x^2 + 8x^3
+	g := []fr.Element{newElem(5), newElem(6), newElem(7), newElem(8)}
+	expected := make([]fr.Element, len(f)+len(g)-1)
+	for i := 0; i < len(expected); i++ {
+		expected[i] = newElem(0)
+	}
+	for i := 0; i < len(f); i++ {
+		for j := 0; j < len(g); j++ {
+			expected[i+j].Add(&expected[i+j], new(fr.Element).Mul(&f[i], &g[j]))
+		}
+	}
+
+	actual := MulPolynomials(f, g)
+	for i := 0; i < len(expected); i++ {
+		if !actual[i].Equal(&expected[i]) {
+			t.Errorf("%d: Expected %s, got %s", i, expected[i].String(), actual[i].String())
+		}
+	}
+}
+
+func newElem(x int64) (z fr.Element) {
+	z.SetInt64(x)
+	return
+}
+
+func elementsString(e []fr.Element) string {
+	s := "["
+	for i := 0; i < len(e); i++ {
+		s += e[i].String()
+		if i != len(e)-1 {
+			s += ", "
+		}
+	}
+	s += "]"
+	return s
+}
